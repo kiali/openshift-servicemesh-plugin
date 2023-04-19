c2dcff40f0656731fdbc922a111333d5a8020dc3
Fernando Hoyos <fhoyosle@redhat.com>
2023-04-19 09:28:40 +0200

    Complete Istio mesh tab functionality

--- plugin/src/components/MeshTab/IstioMesh.tsx
+++ plugin/src/components/MeshTab/IstioMesh.tsx
@@ -1,9 +1,7 @@
 import {
+  aceOptions,
   AceValidations,
   Annotation,
-  deleteIstioConfigDetail,
-  EditorPreview,
-  ErrorMsg,
   getErrorString,
   getIstioConfigDetail,
   getIstioObject,
@@ -14,6 +12,8 @@ import {
   mergeJsonPatch,
   ObjectReference,
   ObjectValidation,
+  parseHelpAnnotations,
+  parseKialiValidations,
   parseLine,
   parseYamlValidations,
   PromisesRegistry,
@@ -23,23 +23,68 @@ import {
   ValidationMessage,
   WorkloadReference
 } from '@kiali/core-ui';
-import { Drawer, DrawerContent, DrawerContentBody, DrawerHead, DrawerPanelContent } from '@patternfly/react-core';
+import {
+  Drawer,
+  DrawerActions,
+  DrawerCloseButton,
+  DrawerContent,
+  DrawerContentBody,
+  DrawerHead,
+  DrawerPanelContent
+} from '@patternfly/react-core';
 import * as React from 'react';
 import { useHistory } from 'react-router';
+import AceEditor from 'react-ace';
+import ReactAce from 'react-ace';
+import 'ace-builds/src-noconflict/mode-yaml';
+import 'ace-builds/src-noconflict/theme-eclipse';
+import 'ace-builds/src-noconflict/ext-searchbox';
 import * as jsYaml from 'js-yaml';
 import { style } from 'typestyle';
 import { getKialiConfig, KialiConfig } from '../../kialiIntegration';
 import { AxiosError } from 'axios';
+import { K8sGroupVersionKind, ResourceLink } from '@openshift-console/dynamic-plugin-sdk';
+import { istioResources } from '../../k8s/resources';
 
 const editorStyle = style({
-  //   width: 'calc(100% - 30px)',
   marginBottom: '15px',
-  border: '1px solid black'
+  border: '1px solid #8b8d8f',
+  $nest: {
+    '& .istio-validation-error': {
+      position: 'absolute',
+      background: 'rgba(204, 0, 0, 0.5)'
+    },
+
+    '& .istio-validation-warning': {
+      position: 'absolute',
+      background: 'rgba(236, 122, 8, 0.5)'
+    },
+
+    '& .istio-validation-info': {
+      position: 'absolute',
+      background: '#d1d1d1'
+    },
+
+    '& div.ace_gutter-cell.ace_info': {
+      // backgroundImage: 'none',
+      // $nest: {
+      //   '&::before': {
+      //     content: '\E92b',
+      //     fontFamily: 'pficon',
+      //     left: '5px',
+      //     position: 'absolute'
+      //   }
+      // }
+    }
+  }
 });
 
 const editorDrawer = style({
-  //   width: 'calc(100% - 30px)',
   margin: '15px'
+});
+
+const drawerStyle = style({
+  zIndex: 0
 });
 
 const configTypes = {
@@ -63,24 +108,20 @@ const IstioConfigMeshTab = () => {
   const namespace = items[0];
   const objectType = configTypes[items[1].substring(items[1].lastIndexOf('~') + 1)].toLowerCase();
   const objectId = items[2];
-  // initKialiListeners();
 
   const [kialiConfig, setKialiConfig] = React.useState<KialiConfig>(undefined);
   const [istioObjectDetails, setIstioObjectDetails] = React.useState<IstioConfigDetails>(undefined);
   const [istioValidations, setIstioValidations] = React.useState<ObjectValidation>(undefined);
-  const [originalIstioObjectDetails, setOriginalIstioObjectDetails] = React.useState<IstioConfigDetails>(undefined);
-  const [originalIstioValidations, setOriginalIstioValidations] = React.useState<ObjectValidation>(undefined);
   const [isModified, setIsModified] = React.useState<boolean>(false);
-  const [isRemoved, setIsRemoved] = React.useState<boolean>(false);
   const [yamlModified, setYamlModified] = React.useState<string>(undefined);
   const [yamlValidations, setYamlValidations] = React.useState<AceValidations>(undefined);
-  const [currentTab, setCurrentTab] = React.useState<string>(undefined);
   const [isExpanded, setIsExpanded] = React.useState<boolean>(true);
   const [selectedEditorLine, setSelectedEditorLine] = React.useState<string>(undefined);
-  const [error, setError] = React.useState<ErrorMsg>(undefined);
-  const [isLoading, setLoading] = React.useState(true);
+  // const [error, setError] = React.useState<ErrorMsg>(undefined);
+  const [load, setLoad] = React.useState(true);
   const promises = new PromisesRegistry();
   const istioAPIEnabled = kialiConfig?.status?.istioEnvironment?.istioAPIEnabled;
+  const editorRef = React.createRef<ReactAce>();
 
   const getStatusMessages = (istioConfigDetails?: IstioConfigDetails): ValidationMessage[] => {
     const istioObject = getIstioObject(istioConfigDetails);
@@ -94,7 +135,7 @@ const IstioConfigMeshTab = () => {
     return true;
   };
 
-  const showCards = (refPresent: boolean, istioStatusMsgs: ValidationMessage[]): boolean => {
+  const getShowCards = (refPresent: boolean, istioStatusMsgs: ValidationMessage[]): boolean => {
     return refPresent || hasOverview() || istioStatusMsgs.length > 0;
   };
 
@@ -118,6 +159,27 @@ const IstioConfigMeshTab = () => {
     return details.help || ([] as HelpMessage[]);
   };
 
+  const getIsExpanded = (istioConfigDetails?: IstioConfigDetails) => {
+    let isExpanded = false;
+    if (istioConfigDetails) {
+      isExpanded = getShowCards(
+        getObjectReferences(istioConfigDetails).length > 0,
+        getStatusMessages(istioConfigDetails)
+      );
+    }
+    return isExpanded;
+  };
+
+  const resizeEditor = () => {
+    if (editorRef.current) {
+      const editor = editorRef.current!['editor'];
+      // The Drawer has an async animation that needs a timeout before to resize the editor
+      setTimeout(() => {
+        editor.resize(true);
+      }, 250);
+    }
+  };
+
   const injectGalleyError = (error: AxiosError): AceValidations => {
     const msg: string[] = getErrorString(error).split(':');
     const errMsg: string = msg.slice(1, msg.length).join(':');
@@ -139,15 +201,6 @@ const IstioConfigMeshTab = () => {
     history.goBack();
   };
 
-  const onDelete = () => {
-    deleteIstioConfigDetail(namespace, objectType, objectId)
-      .then(() => history.goBack())
-      .catch(error => {
-        console.error('Could not delete IstioConfig details.');
-        // AlertUtils.addError('Could not delete IstioConfig details.', error);
-      });
-  };
-
   const onUpdate = () => {
     jsYaml.safeLoadAll(yamlModified, (objectModified: object) => {
       const jsonPatch = JSON.stringify(mergeJsonPatch(objectModified, getIstioObject(istioObjectDetails))).replace(
@@ -159,15 +212,19 @@ const IstioConfigMeshTab = () => {
           const targetMessage = `${namespace} / ${objectType} / ${objectId}`;
           console.log(targetMessage);
           //   AlertUtils.add('Changes applied on ' + targetMessage, 'default', MessageType.SUCCESS);
-          // TODO Load istio object details
-          //   fetchIstioObjectDetails();
+          setLoad(true);
         })
         .catch(error => {
           //   AlertUtils.addError('Could not update IstioConfig details.', error);
-          console.error('Could not update IstioConfig details.');
+          console.error('Could not update IstioConfig details.' + error);
           setYamlValidations(injectGalleyError(error));
         });
     });
+  };
+
+  const onDrawerClose = () => {
+    setIsExpanded(false);
+    resizeEditor();
   };
 
   const onRefresh = () => {
@@ -176,14 +233,40 @@ const IstioConfigMeshTab = () => {
       refresh = window.confirm('You have unsaved changes, are you sure you want to refresh ?');
     }
     if (refresh) {
-      // TODO Load istio object details
-      //   fetchIstioObjectDetails();
+      setLoad(true);
     }
+  };
+
+  // Aux function to calculate rows for 'status' and 'managedFields' which are typically folded
+  const getFoldRanges = (yaml: string | undefined): any => {
+    const range = {
+      startRow: -1,
+      endRow: -1
+    };
+
+    if (yaml) {
+      const ylines = yaml.split('\n');
+      ylines.forEach((line: string, i: number) => {
+        // Counting spaces to check managedFields, yaml is always processed with that structure so this is safe
+        if (line.startsWith('status:') || line.startsWith('  managedFields:')) {
+          if (range.startRow === -1) {
+            range.startRow = i;
+          } else if (range.startRow > i) {
+            range.startRow = i;
+          }
+        }
+        if (line.startsWith('spec:') && range.startRow !== -1) {
+          range.endRow = i;
+        }
+      });
+    }
+
+    return range;
   };
 
   const onDrawerToggle = () => {
     setIsExpanded(!isExpanded);
-    //   () => this.resizeEditor()
+    resizeEditor();
   };
 
   const fetchYaml = () => {
@@ -191,7 +274,7 @@ const IstioConfigMeshTab = () => {
       return yamlModified;
     }
     const istioObject = getIstioObject(istioObjectDetails);
-    return istioObject ? jsYaml.dump(istioObject, safeDumpOptions) : '';
+    return istioObject ? jsYaml.safeDump(istioObject, safeDumpOptions) : '';
   };
 
   const onEditorChange = (value: string) => {
@@ -217,6 +300,28 @@ const IstioConfigMeshTab = () => {
     );
   };
 
+  const linkTemplate = (name: string, namespace: string, objectType: string) => {
+    let groupVersionKind: K8sGroupVersionKind;
+
+    switch (objectType) {
+      case 'service':
+        groupVersionKind = { group: '', version: 'v1', kind: 'Service' };
+        break;
+      // TODO add objectType in workloadReferences to know exact workload type
+      case 'workload':
+        groupVersionKind = { group: 'apps', version: 'v1', kind: 'Deployment' };
+        break;
+      case 'gateway':
+        groupVersionKind = { group: 'networking.istio.io', version: 'v1beta1', kind: 'Gateway' };
+        break;
+      default:
+        groupVersionKind = istioResources.find(resource => resource.kind.toLowerCase() === objectType);
+        break;
+    }
+
+    return <ResourceLink groupVersionKind={groupVersionKind} name={name} namespace={namespace} />;
+  };
+
   React.useEffect(() => {
     getKialiConfig()
       .then(kialiConfig => {
@@ -226,36 +331,53 @@ const IstioConfigMeshTab = () => {
   }, []);
 
   React.useEffect(() => {
-    if (kialiConfig) {
+    if (kialiConfig && load) {
       fetchIstioConfigs()
         .then(istioConfigDetail => {
           setIstioObjectDetails(istioConfigDetail);
-          setOriginalIstioObjectDetails(istioConfigDetail);
           setIstioValidations(istioConfigDetail.validation);
-          setOriginalIstioValidations(istioConfigDetail.validation);
           setIsModified(false);
-          // setIsExpanded(isExpanded(istioConfigDetail));
-          // setYamlModified(''),
-          //   resizeEditor()
+          setIsExpanded(getIsExpanded(istioConfigDetail));
+          setYamlModified('');
+          resizeEditor();
         })
         .catch(error => {
-          const msg: ErrorMsg = {
-            title: 'No Istio object is selected',
-            description: objectId + ' is not found in the mesh'
-          };
-          setIsRemoved(true);
-          setError(msg);
+          // const msg: ErrorMsg = {
+          //   title: 'No Istio object is selected',
+          //   description: objectId + ' is not found in the mesh'
+          // };
+          // setError(msg);
           //   AlertUtils.addError(
           //     `Could not fetch Istio object type [${props.objectType}] name [${props.object}] in namespace [${props.namespace}].`,
           //     error
           //   );
           console.error(
-            `Could not fetch Istio object type [${objectType}] name [${objectId}] in namespace [${namespace}].`
+            `Could not fetch Istio object type [${objectType}] name [${objectId}] in namespace [${namespace}]. ${error}`
           );
+        })
+        .finally(() => {
+          setLoad(false);
         });
     }
-  }, [kialiConfig]);
+  }, [kialiConfig, load]);
 
+  React.useEffect(() => {
+    // Hack to force redisplay of annotations after update
+    // See https://github.com/securingsincity/react-ace/issues/300
+    if (editorRef.current) {
+      const editor = editorRef.current!['editor'];
+
+      editor.onChangeAnnotation();
+
+      // Fold status and/or managedFields fields
+      const { startRow, endRow } = getFoldRanges(fetchYaml());
+      if (!isModified) {
+        editor.session.foldAll(startRow, endRow, 0);
+      }
+    }
+  });
+
+  const yamlSource = fetchYaml();
   const istioStatusMsgs = getStatusMessages(istioObjectDetails);
 
   const objectReferences = getObjectReferences(istioObjectDetails);
@@ -263,7 +385,24 @@ const IstioConfigMeshTab = () => {
   const workloadReferences = getWorkloadReferences(istioObjectDetails);
   const helpMessages = getHelpMessages(istioObjectDetails);
 
-  const yamlSource = fetchYaml();
+  const refPresent = objectReferences.length > 0;
+  const showCards = getShowCards(refPresent, istioStatusMsgs);
+
+  let editorValidations: AceValidations = {
+    markers: [],
+    annotations: []
+  };
+  if (!isModified) {
+    editorValidations = parseKialiValidations(yamlSource, istioValidations);
+  } else {
+    if (yamlValidations) {
+      editorValidations.markers = yamlValidations.markers;
+      editorValidations.annotations = yamlValidations.annotations;
+    }
+  }
+
+  const helpAnnotations = parseHelpAnnotations(yamlSource, helpMessages);
+  helpAnnotations.forEach(ha => editorValidations.annotations.push(ha));
 
   const panelContent = (
     <DrawerPanelContent>
@@ -279,10 +418,13 @@ const IstioConfigMeshTab = () => {
             workloadReferences={workloadReferences}
             helpMessages={helpMessages}
             selectedLine={selectedEditorLine}
-            // kiosk={this.props.kiosk}
             istioAPIEnabled={istioAPIEnabled}
+            linkTemplate={linkTemplate}
           />
         )}
+        <DrawerActions>
+          <DrawerCloseButton onClick={onDrawerClose} />
+        </DrawerActions>
       </DrawerHead>
     </DrawerPanelContent>
   );
@@ -291,21 +433,29 @@ const IstioConfigMeshTab = () => {
 
   const editor = istioObjectDetails ? (
     <div className={editorDrawer}>
-      <div className={editorStyle}>
-        <EditorPreview
-          onChange={onEditorChange}
-          yaml={istioObjectDetails ? yamlSource : undefined}
-          onCursorChange={onCursorChange}
-        />
-      </div>
+      <AceEditor
+        ref={editorRef}
+        mode="yaml"
+        theme="eclipse"
+        onChange={onEditorChange}
+        width={'100%'}
+        className={editorStyle}
+        wrapEnabled={true}
+        readOnly={!canUpdate()}
+        setOptions={aceOptions}
+        value={istioObjectDetails ? yamlSource : undefined}
+        annotations={editorValidations.annotations}
+        markers={editorValidations.markers}
+        onCursorChange={onCursorChange}
+      />
       <IstioActionButtons
         objectName={objectId}
         readOnly={!canUpdate()}
-        canUpdate={canUpdate() && isModified && !isRemoved && !yamlErrors}
+        canUpdate={canUpdate() && isModified && !yamlErrors}
         onCancel={onCancel}
         onUpdate={onUpdate}
         onRefresh={onRefresh}
-        showOverview={false}
+        showOverview={showCards}
         overview={isExpanded}
         onOverview={onDrawerToggle}
       />
@@ -313,13 +463,11 @@ const IstioConfigMeshTab = () => {
   ) : null;
 
   return (
-    <>
-      <Drawer isExpanded={isExpanded} isInline={true}>
-        <DrawerContent panelContent={showCards ? panelContent : undefined}>
-          <DrawerContentBody>{editor}</DrawerContentBody>
-        </DrawerContent>
-      </Drawer>
-    </>
+    <Drawer className={drawerStyle} isExpanded={isExpanded} isInline={true}>
+      <DrawerContent panelContent={showCards ? panelContent : undefined}>
+        <DrawerContentBody>{editor}</DrawerContentBody>
+      </DrawerContent>
+    </Drawer>
   );
 };
 
