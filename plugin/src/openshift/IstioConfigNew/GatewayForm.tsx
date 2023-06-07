17376cd638487f67d5261097fe59cb8ebd86d8e2
Fernando Hoyos <fhoyosle@redhat.com>
2023-06-07 10:11:05 +0200

    Remove unused new forms

--- plugin/src/openshift/IstioConfigNew/GatewayForm.tsx
+++
@@ -1,8 +0,0 @@
-import * as React from 'react';
-import { IstioConfigNewForm } from './IstioConfigNewForm';
-
-const GatewayForm = () => {
-  return <IstioConfigNewForm objectType={'Gateway'}>Gateway Form</IstioConfigNewForm>;
-};
-
-export default GatewayForm;
--- plugin/src/openshift/IstioConfigNew/K8sGatewayForm.tsx
+++
@@ -1,8 +0,0 @@
-import * as React from 'react';
-import { IstioConfigNewForm } from './IstioConfigNewForm';
-
-const GatewayForm = () => {
-  return <IstioConfigNewForm objectType={'K8sGateway'}>K8s Gateway Form</IstioConfigNewForm>;
-};
-
-export default GatewayForm;
