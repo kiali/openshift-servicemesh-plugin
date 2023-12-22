063e093e377a5c09907ce9485a143beaa5f7abd0
ScriptingShrimp <scriptingshrimp@protonmail.com>
2023-12-22 15:48:44 +0100

    removed some boilerplate lines

--- plugin/cypress/support/commands.ts
+++ plugin/cypress/support/commands.ts
@@ -8,30 +8,11 @@
 // commands please read more here:
 // https://on.cypress.io/custom-commands
 // ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
-//
+
 declare global {
     namespace Cypress {
         interface Chainable {
             login(OC_CLUSTER_USER: string, OC_CLUSTER_PASS: string, OC_IDP: string): Chainable<void>
-            //   drag(subject: string, options?: Partial<TypeOptions>): Chainable<Element>
-            //   dismiss(subject: string, options?: Partial<TypeOptions>): Chainable<Element>
-            //   visit(originalFn: CommandOriginalFn, url: string, options: Partial<VisitOptions>): Chainable<Element>
         }
     }
 }
