f309a8bb74179ce09e415b0dacbd538c7cbb1e98
Fernando Hoyos <fhoyosle@redhat.com>
2024-01-30 12:51:59 +0100

    Retrieve hack scripts from Kiali repository

---
+++ hack/download-hack-scripts.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+#############################################################
+# This sripts donwloads hack scripts from Kiali repository
+#############################################################
+
+# exit immediately on error
+set -eu
+
+# Where this script is located.
+# It is assumed this directory is inside the same plugin dest repo where the files are to be copied.
+SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd -P)"
+
+# Relative directory inside the local OSSM repo where the istio hack files will be copied.
+DEST_DIR="kiali"
+
+# This is to be the top-level directory of the local OSSM git repo.
+# This is where DEST_DIR should be located.
+DEST_REPO="${SCRIPT_DIR}/.."
+
+# The absolute destination directory
+ABS_DEST_DIR="${DEST_REPO}/${DEST_DIR}"
+
+# Delete existing kiali folder before download hack scripts
+rm -rf ${ABS_DEST_DIR}/{*,.[!.]*}
+
+# Clone kiali repo into kiali folder (no-checkout option to avoid download whole repository)
+git clone --filter=tree:0 --no-checkout --depth 1 --sparse https://github.com/kiali/kiali.git ${ABS_DEST_DIR}
+
+cd ${ABS_DEST_DIR}
+
+# Only download hack/istio folder
+git sparse-checkout set --no-cone hack/istio
+
+git checkout
--- plugin/cypress/support/step_definitions/hooks.ts
+++ plugin/cypress/support/step_definitions/hooks.ts
@@ -19,56 +19,60 @@ const install_demoapp = (demoapp: string): void => {
     istio = '';
   }
 
-  cy.exec(`../hack/istio/cypress/${demoapp}-status.sh`, { failOnNonZeroExit: false, timeout: 120000 }).then(result => {
-    cy.log(result.stdout);
+  cy.exec(`../hack/download-hack-scripts.sh`, { failOnNonZeroExit: false, timeout: 120000 }).then(() => {
+    cy.log(`Get status of ${demoapp} demo app:`);
 
-    if (result.code === 0) {
-      cy.log(`${demoapp} demo app is up and running`);
-    } else {
-      cy.log(`${demoapp} demo app is either broken or not present. Installing now.`);
-      cy.log(`Detecting pod architecture.`);
-
-      cy.exec('../hack/istio/cypress/get-node-architecture.sh', { failOnNonZeroExit: false }).then(result => {
+    cy.exec(`../kiali/hack/istio/cypress/${demoapp}-status.sh`, { failOnNonZeroExit: false, timeout: 120000 }).then(
+      result => {
         if (result.code === 0) {
-          const arch: string = result.stdout;
-          cy.log(`Installing apps on ${arch} architecture.`);
-
-          // is the suite running on openshift?
-          cy.exec('kubectl api-versions | grep --quiet "route.openshift.io";', { failOnNonZeroExit: false }).then(
-            result => {
-              if (result.code === 0) {
-                cy.log('Openshift detected.').log(`Removing old ${demoapp} installations.`);
-
-                cy.exec(`../hack/istio/install-${demoapp}-demo.sh ${deletion} true`).then(() => {
-                  cy.log('Installing new demo app.');
-                  cy.exec(`../hack/istio/install-${demoapp}-demo.sh ${tg} ${istio} -a ${arch}`, {
-                    timeout: 300000
-                  }).then(() => {
-                    cy.log('Waiting for demoapp to be ready.');
-
-                    cy.exec(`../hack/istio/wait-for-namespace.sh -n ${namespaces}`, { timeout: 400000 });
-                  });
-                });
-              } else {
-                cy.log(`Removing old ${demoapp} installations.`)
-                  .exec(`../hack/istio/install-${demoapp}-demo.sh ${deletion} true -c kubectl`)
-                  .then(() => {
-                    cy.log('Installing new demo app.');
-
-                    cy.exec(`../hack/istio/install-${demoapp}-demo.sh -c kubectl ${tg} ${istio} -a ${arch}`, {
-                      timeout: 300000
-                    });
-                  });
-              }
-            }
-          );
+          cy.log(`${demoapp} demo app is up and running`);
         } else {
-          cy.log(
-            'Different architectures on various nodes detected. Failed to install the demoapp using the Cypress hook.'
-          );
+          cy.log(`${demoapp} demo app is either broken or not present. Installing now.`);
+          cy.log(`Detecting pod architecture.`);
+
+          cy.exec('../kiali/hack/istio/cypress/get-node-architecture.sh', { failOnNonZeroExit: false }).then(result => {
+            if (result.code === 0) {
+              const arch: string = result.stdout;
+              cy.log(`Installing apps on ${arch} architecture.`);
+
+              // is the suite running on openshift?
+              cy.exec('kubectl api-versions | grep --quiet "route.openshift.io";', { failOnNonZeroExit: false }).then(
+                result => {
+                  if (result.code === 0) {
+                    cy.log('Openshift detected.').log(`Removing old ${demoapp} installations.`);
+
+                    cy.exec(`../kiali/hack/istio/install-${demoapp}-demo.sh ${deletion} true`).then(() => {
+                      cy.log('Installing new demo app.');
+                      cy.exec(`../kiali/hack/istio/install-${demoapp}-demo.sh ${tg} ${istio} -a ${arch}`, {
+                        timeout: 300000
+                      }).then(() => {
+                        cy.log('Waiting for demoapp to be ready.');
+
+                        cy.exec(`../kiali/hack/istio/wait-for-namespace.sh -n ${namespaces}`, { timeout: 400000 });
+                      });
+                    });
+                  } else {
+                    cy.log(`Removing old ${demoapp} installations.`)
+                      .exec(`../kiali/hack/istio/install-${demoapp}-demo.sh ${deletion} true -c kubectl`)
+                      .then(() => {
+                        cy.log('Installing new demo app.');
+
+                        cy.exec(`../kiali/hack/istio/install-${demoapp}-demo.sh -c kubectl ${tg} ${istio} -a ${arch}`, {
+                          timeout: 300000
+                        });
+                      });
+                  }
+                }
+              );
+            } else {
+              cy.log(
+                'Different architectures on various nodes detected. Failed to install the demoapp using the Cypress hook.'
+              );
+            }
+          });
         }
-      });
-    }
+      }
+    );
   });
 };
 
