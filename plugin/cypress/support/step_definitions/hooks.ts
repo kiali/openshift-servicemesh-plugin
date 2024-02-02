05dc37838e42158d4847a3ab9cc56bc7c8c8375e
Fernando Hoyos <fhoyosle@redhat.com>
2024-02-02 10:28:00 +0100

    Simplify hook file

--- plugin/cypress/support/step_definitions/hooks.ts
+++ plugin/cypress/support/step_definitions/hooks.ts
@@ -1,8 +1,5 @@
 import { Before } from '@badeball/cypress-cucumber-preprocessor';
 
-const CLUSTER1_CONTEXT = Cypress.env('CLUSTER1_CONTEXT');
-const CLUSTER2_CONTEXT = Cypress.env('CLUSTER2_CONTEXT');
-
 const install_demoapp = (demoapp: string): void => {
   let namespaces = 'bookinfo';
   let deletion = `--delete-${demoapp}`;
@@ -22,53 +19,34 @@ const install_demoapp = (demoapp: string): void => {
   const kialiHacksPath = '../_output/kiali/hack/istio';
 
   cy.exec(`../hack/download-hack-scripts.sh`, { failOnNonZeroExit: false, timeout: 120000 }).then(() => {
-    cy.log(`Get status of ${demoapp} demo app:`);
+    cy.log(`Get status of ${demoapp} app:`);
 
     cy.exec(`${kialiHacksPath}/cypress/${demoapp}-status.sh`, { failOnNonZeroExit: false, timeout: 120000 }).then(
       result => {
         if (result.code === 0) {
-          cy.log(`${demoapp} demo app is up and running`);
+          cy.log(`${demoapp} app is up and running`);
         } else {
-          cy.log(`${demoapp} demo app is either broken or not present. Installing now.`);
+          cy.log(`${demoapp} app is either broken or not present. Installing now.`);
           cy.log(`Detecting pod architecture.`);
 
           cy.exec(`${kialiHacksPath}/cypress/get-node-architecture.sh`, { failOnNonZeroExit: false }).then(result => {
             if (result.code === 0) {
               const arch: string = result.stdout;
-              cy.log(`Installing apps on ${arch} architecture.`);
+              cy.log(`Removing old ${demoapp} installations.`);
 
-              // is the suite running on openshift?
-              cy.exec('kubectl api-versions | grep --quiet "route.openshift.io";', { failOnNonZeroExit: false }).then(
-                result => {
-                  if (result.code === 0) {
-                    cy.log('Openshift detected.').log(`Removing old ${demoapp} installations.`);
+              cy.exec(`${kialiHacksPath}/install-${demoapp}-demo.sh ${deletion} true`).then(() => {
+                cy.log(`Installing new ${demoapp} app on ${arch} architecture.`);
+                cy.exec(`${kialiHacksPath}/install-${demoapp}-demo.sh ${tg} ${istio} -a ${arch}`, {
+                  timeout: 300000
+                }).then(() => {
+                  cy.log(`Waiting for ${demoapp} app to be ready.`);
 
-                    cy.exec(`${kialiHacksPath}/install-${demoapp}-demo.sh ${deletion} true`).then(() => {
-                      cy.log('Installing new demo app.');
-                      cy.exec(`${kialiHacksPath}/install-${demoapp}-demo.sh ${tg} ${istio} -a ${arch}`, {
-                        timeout: 300000
-                      }).then(() => {
-                        cy.log('Waiting for demoapp to be ready.');
-
-                        cy.exec(`${kialiHacksPath}/wait-for-namespace.sh -n ${namespaces}`, { timeout: 400000 });
-                      });
-                    });
-                  } else {
-                    cy.log(`Removing old ${demoapp} installations.`)
-                      .exec(`${kialiHacksPath}/install-${demoapp}-demo.sh ${deletion} true -c kubectl`)
-                      .then(() => {
-                        cy.log('Installing new demo app.');
-
-                        cy.exec(`${kialiHacksPath}/install-${demoapp}-demo.sh -c kubectl ${tg} ${istio} -a ${arch}`, {
-                          timeout: 300000
-                        });
-                      });
-                  }
-                }
-              );
+                  cy.exec(`${kialiHacksPath}/wait-for-namespace.sh -n ${namespaces}`, { timeout: 400000 });
+                });
+              });
             } else {
               cy.log(
-                'Different architectures on various nodes detected. Failed to install the demoapp using the Cypress hook.'
+                `Different architectures on various nodes detected. Failed to install the ${demoapp} app using the Cypress hook.`
               );
             }
           });
@@ -78,50 +56,6 @@ const install_demoapp = (demoapp: string): void => {
   });
 };
 
-Before({ tags: '@gateway-api' }, () => {
-  cy.exec('kubectl get crd gateways.gateway.networking.k8s.io', { failOnNonZeroExit: false }).then(result => {
-    if (result.code !== 0) {
-      cy.log('Gateway API not found. Enabling it now.');
-
-      cy.exec(
-        'kubectl kustomize "github.com/kubernetes-sigs/gateway-api/config/crd/experimental?ref=v1.0.0" | kubectl apply -f -;'
-      )
-        .its('code')
-        .should('eq', 0);
-    }
-  });
-});
-
 Before({ tags: '@bookinfo-app' }, () => {
   install_demoapp('bookinfo');
-});
-
-Before({ tags: '@error-rates-app' }, () => {
-  install_demoapp('error-rates');
-});
-
-Before({ tags: '@sleep-app' }, () => {
-  install_demoapp('sleep');
-});
-
-Before({ tags: '@remote-istio-crds' }, () => {
-  cy.exec(
-    `kubectl get crd --context ${CLUSTER2_CONTEXT} -o=custom-columns=NAME:.metadata.name |  grep -E -i '.(istio|k8s).io$'`,
-    { failOnNonZeroExit: false }
-  ).then(result => {
-    if (result.code !== 0) {
-      cy.log('Istio CRDs not found on the remote cluster. Enabling it now.');
-
-      cy.exec(
-        `kubectl apply -f https://raw.githubusercontent.com/istio/istio/master/manifests/charts/base/crds/crd-all.gen.yaml --context ${CLUSTER2_CONTEXT}`
-      )
-        .its('code')
-        .should('eq', 0)
-        .then(() => {
-          cy.exec(
-            `kubectl rollout restart deployment/kiali -n istio-system --context ${CLUSTER1_CONTEXT} && kubectl rollout status deployment/kiali -n istio-system --context ${CLUSTER1_CONTEXT}`
-          );
-        });
-    }
-  });
 });
