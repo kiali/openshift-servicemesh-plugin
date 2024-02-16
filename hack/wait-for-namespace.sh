c5087998bdb19818d67dcbce595a06eca0be1a89
Fernando Hoyos <fhoyosle@redhat.com>
2024-02-16 09:00:09 +0100

    Don't abort if pods are not ready

--- hack/wait-for-namespace.sh
+++ hack/wait-for-namespace.sh
@@ -67,7 +67,7 @@ if [ "${IS_MAISTRA}" == "false" ]; then
 fi
 
 for NAMESPACE in ${NAMESPACES[@]}; do
-  oc wait --for=condition=Ready pods --all -n "$NAMESPACE" --timeout 60s
+  oc wait --for=condition=Ready pods --all -n "$NAMESPACE" --timeout 60s || true
 done
 
 sleep 80
