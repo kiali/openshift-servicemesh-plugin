aecb9ac6bc471817a2803dd5918dc59c8af37fcb
Fernando Hoyos <fhoyosle@redhat.com>
2024-03-06 12:34:05 +0100

    Create ossmc i18n files and combine with kiali ones

---
+++ plugin/src/openshift/utils/MenuTitles.ts
@@ -0,0 +1,7 @@
+import i18next from 'i18next';
+
+// Dummy OSSMC menu titles to be detected by i18next parser tool and added to i18n locale file
+export const serviceMesh = i18next.t('Service Mesh');
+export const overview = i18next.t('Overview');
+export const graph = i18next.t('Graph');
+export const istioConfig = i18next.t('Istio Config');
