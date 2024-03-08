7d24e445dd957c5af9a004d878bffefdbe650072
Fernando Hoyos <122779323+ferhoyos@users.noreply.github.com>
2024-03-08 14:02:19 +0100

    Adapt Cypress tests to OCP 4.15 (#275)

--- plugin/cypress/support/commands.ts
+++ plugin/cypress/support/commands.ts
@@ -1,4 +1,7 @@
 /// <reference types="cypress" />
+
+import { isLocalhost } from './utils';
+
 // ***********************************************
 // This example commands.ts shows you how to
 // create various custom commands and overwrite
@@ -18,17 +21,20 @@ declare global {
 }
 
 Cypress.Commands.add('login', (OC_CLUSTER_USER, OC_CLUSTER_PASS, OC_IDP) => {
-  const user = OC_CLUSTER_USER || Cypress.env('OC_CLUSTER_USER');
-  const password = OC_CLUSTER_PASS || Cypress.env('OC_CLUSTER_PASS');
-  const idp = OC_IDP || Cypress.env('OC_IDP');
+  // Openshift Console from localhost does not have login page
+  if (!isLocalhost()) {
+    const user = OC_CLUSTER_USER || Cypress.env('OC_CLUSTER_USER');
+    const password = OC_CLUSTER_PASS || Cypress.env('OC_CLUSTER_PASS');
+    const idp = OC_IDP || Cypress.env('OC_IDP');
 
-  cy.visit('/').then(() => {
-    cy.log('OC_IDP: ', typeof idp, JSON.stringify(idp));
-    if (idp != undefined) {
-      cy.get('.pf-c-button').contains(idp).click();
-    }
-    cy.get('#inputUsername').clear().type(user);
-    cy.get('#inputPassword').clear().type(password);
-    cy.get('button[type="submit"]').click();
-  });
+    cy.visit('/').then(() => {
+      cy.log('OC_IDP: ', typeof idp, JSON.stringify(idp));
+      if (idp != undefined) {
+        cy.get('.pf-c-button').contains(idp).click();
+      }
+      cy.get('#inputUsername').clear().type(user);
+      cy.get('#inputPassword').clear().type(password);
+      cy.get('button[type="submit"]').click();
+    });
+  }
 });
