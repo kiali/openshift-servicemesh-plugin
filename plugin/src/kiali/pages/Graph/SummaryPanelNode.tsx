2145dcc9c04e46308368f492bd10626d6ffe1b04
Mayleigh Tjapkes <mamyers@redhat.com>
2025-09-30 08:41:11 -0400

    Only show when Netobserv is installed

--- plugin/src/kiali/pages/Graph/SummaryPanelNode.tsx
+++ plugin/src/kiali/pages/Graph/SummaryPanelNode.tsx
@@ -235,21 +235,23 @@ export class SummaryPanelNodeComponent extends React.Component<SummaryPanelNodeC
               )}
 
               {secondBadge}
-              <div className={nodeInfoStyle}>
-                <NetworkTrafficBadge namespace={nodeData.namespace} />
-                {this.props.netObsurl ? (
-                  <a href={this.props.netObsurl}>
-                    Network Traffic
-                  </a>
-                ) : (
-                  <KialiPageLink
-                    href={`/graph/namespaces?namespaces=${encodeURIComponent(nodeData.namespace)}`}
-                    cluster={nodeData.cluster}
-                  >
-                    Network Traffic
-                  </KialiPageLink>
-                )}
-              </div>
+              {networkTrafficPluginConfig && (
+                <div className={nodeInfoStyle}>
+                  <NetworkTrafficBadge namespace={nodeData.namespace} />
+                  {this.props.netObsurl ? (
+                    <a href={this.props.netObsurl}>
+                      network traffic
+                    </a>
+                  ) : (
+                    <KialiPageLink
+                      href={`/graph/namespaces?namespaces=${encodeURIComponent(nodeData.namespace)}`}
+                      cluster={nodeData.cluster}
+                    >
+                      network traffic
+                    </KialiPageLink>
+                  )}
+                </div>
+              )}
               {!nodeData.isWaypoint && (
                 <div className={nodeInfoStyle}>
                   {renderBadgedLink(nodeData)}
