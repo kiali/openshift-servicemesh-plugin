c6b586966b5adf0ed52e5a8e06310cbdd02a03ec
Fernando Hoyos <fhoyosle@redhat.com>
2026-04-17 08:50:59 +0200

    Copy of Kiali frontend source code Kiali frontend source originated from: * git ref:    PR 9563 * git commit: d1d465747a5ebfaa6c4b961d33990a28858253c5 * GitHub URL: https://github.com/kiali/kiali/tree/d1d465747a5ebfaa6c4b961d33990a28858253c5/frontend/src
    Signed-off-by: Fernando Hoyos <fhoyosle@redhat.com>

--- plugin/src/kiali/components/Link/KialiLink.tsx
+++ plugin/src/kiali/components/Link/KialiLink.tsx
@@ -1,14 +1,16 @@
 import * as React from 'react';
 import { Button } from '@patternfly/react-core';
 import { Link } from 'react-router-dom-v5-compat';
-import { isParentKiosk, kioskContextMenuAction } from '../Kiosk/KioskActions';
+import { isParentKiosk, kioskNavigateAction } from '../Kiosk/KioskActions';
 import { useKialiSelector } from '../../hooks/redux';
 
 type KialiLinkProps = {
   children: React.ReactNode;
   className?: string;
   dataTest?: string;
+  id?: string;
   onClick?: () => void;
+  style?: React.CSSProperties;
   to: string;
 };
 
@@ -18,16 +20,31 @@ export const KialiLink: React.FC<KialiLinkProps> = (props: KialiLinkProps) => {
   const handleClick = (): void => {
     props.onClick?.();
     if (isParentKiosk(kiosk)) {
-      kioskContextMenuAction(props.to);
+      kioskNavigateAction(props.to);
     }
   };
 
   return isParentKiosk(kiosk) ? (
-    <Button variant="link" isInline onClick={handleClick} data-test={props.dataTest} data-href={props.to}>
+    <Button
+      variant="link"
+      isInline
+      onClick={handleClick}
+      data-test={props.dataTest}
+      data-href={props.to}
+      id={props.id}
+      style={props.style}
+    >
       <span className={props.className}>{props.children}</span>
     </Button>
   ) : (
-    <Link to={props.to} className={props.className} data-test={props.dataTest} onClick={props.onClick}>
+    <Link
+      to={props.to}
+      className={props.className}
+      data-test={props.dataTest}
+      id={props.id}
+      onClick={props.onClick}
+      style={props.style}
+    >
       {props.children}
     </Link>
   );
